2fabc7872b5affcf71bbd6f7f89651cda822fb0e
Nikita <nikitakuril329@gmail.com>
2023-10-11 13:14:00 +0530

    10

---
+++ javascript/03display-conten.js
@@ -0,0 +1,5 @@
+window.alert("window alert...!");
+window.confirm("are you sure?");
+window.prompt("5+5=?");
+
+window.print();
---
+++ javascript/jaydeep.js
@@ -0,0 +1,8 @@
+document.write("lkkndv(*& <i>js</i>)");
+console.log("consol....!")
+document.getElementById('d1').innerHTML="ksjfgkjhghj"
+
+window.alert("dskjth")
+confirm("lskfhkj")
+prompt("lksdfhk")
+print();
