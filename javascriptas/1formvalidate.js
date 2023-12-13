bf4c50229e61b351aac325d4242869805ff1c563
Nikita <nikitakuril329@gmail.com>
2023-12-13 11:28:19 +0530

    12

--- javascriptas/1formvalidate.js
+++ javascriptas/1formvalidate.js
@@ -1,11 +1,11 @@
 function validateform() {
   var name1 = document.getElementById("inp1").value;
-  var password = document.myform.password.value;
+  var password1 = document.getElementById("inp2").value;
 
   if (name1 == null || name1 == "") {
-    alert("name can not be blank");
+    alert("your name enter");
     return false;
-  } else if (Password.length < 6) {
+  } else if (Password1.length < 6) {
     alert("password must be at least 6 characters long.");
     return false;
   }
